d13ac55f26927d04a30bb2ece54114778dd95e0d
everan-1994 <everan@aliyun.com>
2019-02-20 14:56:23 +0800

    fix push message bug

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -7,8 +7,10 @@ class WeChatController extends Controller
     public function serve()
     {
         $app = app('wechat.official_account');
-        $app->server->push(function($message){
-            return "欢迎关注 everan！你发的信息为：" . $message;
+
+        $app->server->push(function ($message) {
+            \Log::info(json_encode($message));
+            return "欢迎关注 everan！你发的信息为：";
         });
 
         return $app->server->serve();
