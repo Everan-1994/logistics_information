5d8c0c6b1c53f68378ae2157488e5cdb3ac06d47
everan-1994 <everan@aliyun.com>
2019-06-11 11:48:48 +0800

    test

--- resources/views/oauth.blade.php
+++ resources/views/oauth.blade.php
@@ -117,8 +117,9 @@
     });
 
     $('#info').click(() => {
+        let code = GetUrlParam('code')
         $.ajax({
-            url: 'https://merchants.lzdu.com/api/get_data',
+            url: 'https://merchants.lzdu.com/api/get_data?code' + code,
             type: 'get',
             dataType: 'json',
             success: (res) => {
@@ -126,6 +127,27 @@
             }
         })
     });
+
+    function GetUrlParam(paraName) {
+　　　　var url = document.location.toString();
+　　　　var arrObj = url.split("?");
+
+　　　　if (arrObj.length > 1) {
+　　　　　　var arrPara = arrObj[1].split("&");
+　　　　　　var arr;
+
+　　　　　　for (var i = 0; i < arrPara.length; i++) {
+　　　　　　　　arr = arrPara[i].split("=");
+
+　　　　　　　　if (arr != null && arr[0] == paraName) {
+　　　　　　　　　　return arr[1];
+　　　　　　　　}
+　　　　　　}
+　　　　　　return "";
+　　　　} else {
+　　　　　　return "";
+　　　　}
+　　}
 </script>
 
 </html>
